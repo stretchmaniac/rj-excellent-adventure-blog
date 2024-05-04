3db31b8fbfff7c8fe00b73ac19ecdbb7919c5930
Alan Koval <alank399@gmail.com>
2024-05-03 20:48:25 -0500

    Include page contents when searching.

--- src/client/tools/page-search.ts
+++ src/client/tools/page-search.ts
@@ -15,6 +15,26 @@ export default function pageContains(page: Page, searchString: string) : boolean
         date2 = month + "/" + day + "/" + year 
         date3 = year + "/" + month + "/" + day
     }
-    const searches = [title, summary, date1, date2, date3]
+    const searches = [title, summary, date1, date2, date3, 
+        ...getPageDesignTextNodes(page.design).map(s => s.toLocaleLowerCase())
+    ]
     return searches.filter(s => s.indexOf(lowerSearch) >= 0).length > 0
+}
+
+function getPageDesignTextNodes(obj: any): string[] {
+    if(Array.isArray(obj)){
+        const res = []
+        for(let el of obj){
+            res.push(...getPageDesignTextNodes(el))
+        }
+        return res
+    }
+    const res = []
+    if('text' in obj && obj.text.trim().length !== 0){
+        res.push(obj.text)
+    }
+    if('children' in obj){
+        res.push(...getPageDesignTextNodes(obj.children))
+    }
+    return res
 }
