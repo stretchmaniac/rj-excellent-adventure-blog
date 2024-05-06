abeb42600d0bafcadf65228501c627100563bbf1
Alan Koval <alank399@gmail.com>
2024-05-06 11:44:28 -0500

    Remove italic default styling from captions.

--- src/client/components/slate/MediaChildBox.tsx
+++ src/client/components/slate/MediaChildBox.tsx
@@ -187,7 +187,7 @@ function toggleParentCaption(editor: Editor, props: MediaChildProps, enabled: bo
             children: [{
                 type: 'paragraph',
                 textAlign: 'center',
-                children: [{text: 'parent caption text', fontSize: 'small', italic: true}]
+                children: [{text: 'parent caption text', fontSize: 'small'}]
             }]
         })
     }
@@ -213,7 +213,7 @@ function toggleCaption(editor: Editor, props: MediaChildProps, enabled: boolean)
             children: [{
                 type: 'paragraph',
                 textAlign: 'center',
-                children: [{text: 'caption text', fontSize: 'small', italic: true}]
+                children: [{text: 'caption text', fontSize: 'small'}]
             }]
         })
     }
